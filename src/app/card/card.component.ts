926b5a76fe0dc039613cac004368cb9fdb892e86
Alires Rosa <alires.cardoso@ufpr.br>
2025-03-30 21:32:17 -0300

    feat: parte final do card finalizado

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+
+  numeros : string[] = ['1','2','3','4','5'];
+  numeroSelecionado : string = '0';
+  ativo : Boolean = true;
+
+  pegar(numero : string){
+    this.numeroSelecionado = numero;
+  }
+
+  trocar(){
+    if(this.numeroSelecionado != '0'){
+      this.ativo = !this.ativo;
+    }
+  }
+  
 
 }
